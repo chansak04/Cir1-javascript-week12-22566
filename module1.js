1e93024e9776f5d10207c13094d612cc87d82b47
chansak04 <deawchansak@gmail.com>
2024-01-04 15:23:26 +0700

    master

---
+++ module1.js
@@ -0,0 +1,4 @@
+// Named Export
+export const department = "Information Technology" // value
+export const task = ["develop", "design", "animation"] //Array
+export const students = { id: 1, name: "Mark", age: 16 };
---
+++ module5.js
@@ -0,0 +1,8 @@
+//Export with alias
+const number = [1,2,3,4,5];
+
+function hello() {
+    console.log('Hello')
+}
+
+export default hello
